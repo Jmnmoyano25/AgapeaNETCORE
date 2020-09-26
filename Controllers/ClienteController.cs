54637c1a3886421e5b9782303e90c5de10858486
juan manuel moyano ceballos <jmnmoyano@hotmail.com>
2020-09-26 16:50:10 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgapeaNETCORE.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        #region  "......PROPIEDADES DE CLASE..........."
+
+        #endregion
+        #region  "......METODOS DE CLASE..........."
+
+       
+
+        #region  "1.......METODOS QUE DEVUELVEN VISTAS (PUBLICOS)..........."
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        #endregion
+        #region  "2.......METODOS PRIVADOS..........."
+
+        #endregion
+        #endregion
+    }
+}
