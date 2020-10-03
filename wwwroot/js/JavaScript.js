7ecd2429845d9e7600f274a552b4d493bfa18f21
juan manuel moyano ceballos <jmnmoyano@hotmail.com>
2020-10-03 17:15:31 +0200

    03-10-2020

---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,69 @@
+﻿
+
+
+
+
+
+
+/*
+
+
+var arrayLista = new Array;
+var lista = new Array;
+lista = document.getElementsByTagName("input");
+var er = /^[a-zA-Z\s0-9@]{2,20}$/;
+var erpass = /^[a-zA-Z0-9]{3,20}$/;
+var b = 0;
+function CajaTexto() {
+    CajaTexto.prototype.identificador = "id" || "";
+    CajaTexto.prototype.valor = "val" || "";
+    CajaTexto.prototype.valida = false;
+}
+for (var a = 0; a < lista.length; a++) {
+    if (lista[a].getAttribute("type") == "text") {
+        console.log("poscion " + a + " ....typo " + lista[a].getAttribute("type"))
+        var caja = new CajaTexto;
+        arrayLista[b] = caja;
+        caja.identificador = lista[a].getAttribute("id");
+        caja.valor = lista[a].value;
+        if (er.test(caja.valor) == true) {
+            caja.valida = true;
+        }
+        b++;
+    }
+}
+//para ver las password
+for (var a = 0; a < lista.length; a++) {
+    if (lista[a].getAttribute("type") == "password") {
+        console.log("poscion " + a + " ....typo " + lista[a].getAttribute("type"))
+        var caja = new CajaTexto;
+        arrayLista[b] = caja;
+        caja.identificador = lista[a].getAttribute("id");
+        caja.valor = lista[a].value;
+        if (erpass.test(caja.valor) == true) {
+            caja.valida = true;
+        }
+        b++;
+    }
+}
+var ok = false
+for (var a = 0; a < arrayLista.length; a++) {
+    if (arrayLista[a].valida == true) {
+        console.log("el valor es: " + arrayLista[a].valida + " con valor: " + arrayLista[a].valor)
+        ok = true
+    } else {
+        console.log("el valor es: " + arrayLista[a].valida + " con valor: " + arrayLista[a].valor)
+        ok = false
+    }
+}
+if (ok == true)
+
+/*<a href="javascript:document.f1.submit()" type="button" class="botonregistra" title="Enviar datos para registrarse"></a>
+
+boton.addEventListener("click", function(ev){
+    window.alert("hola, vamos a cancelar el submit()");
+    ev.preventDefault();
+});
+
+
+*/
